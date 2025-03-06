7a29fc4d145ecc505f4e6b4f127efa0ed46745e5
Ahsan3991 <ahsanbashir2012@gmail.com>
2025-03-06 23:10:51 +0100

    sss

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ def login_page():
         left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, 0.4);  /* Dark transparent overlay */
+        background: rgba(0, 0, 0, 0.8);  /* Dark transparent overlay */
         z-index: -1;  /* Places it behind content */
     }}
     </style>
