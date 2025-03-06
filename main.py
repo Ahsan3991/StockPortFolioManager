d616cdcd56931a14a496977d3c1e8ca1ede830fe
Ahsan3991 <ahsanbashir2012@gmail.com>
2025-03-06 23:31:38 +0100

    checks

--- main.py
+++ main.py
@@ -277,9 +277,10 @@ def login_page():
             background-color: rgba(25, 26, 22, 0.8);
             
         }}
-        .st-emotion-cache-1je36yt{{
-            margin: 0 auto;
-            padding-top: 20px
+        .st-emotion-cache-1v0mbdj{{
+            position: relative; 
+            z-index: 1;
+            background-color: rgba(25, 26, 22, 0.9);
         }}
 
         .st-emotion-cache-6sdwd6 {{
