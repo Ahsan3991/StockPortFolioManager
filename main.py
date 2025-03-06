4ba838aa6d8f23eca83755eb4b7d7885f09d0578
Ahsan3991 <ahsanbashir2012@gmail.com>
2025-03-06 23:17:56 +0100

    check

--- main.py
+++ main.py
@@ -239,7 +239,6 @@ def login_page():
     .st-emotion-cache-uf99v8 {{
         background-image: url("https://raw.githubusercontent.com/Ahsan3991/StockPortFolioManager/refs/heads/testing/assets/wealthwise-logo-zip-file/background-image.png");
         background-size: cover;
-        background-position: center;
         position: relative;
         z-index: -1;
         
