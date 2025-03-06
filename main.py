dc9097c7b06c7acfc900fffd8c7bc2513d805a41
Ahsan3991 <ahsanbashir2012@gmail.com>
2025-03-06 23:13:14 +0100

    sssac

--- main.py
+++ main.py
@@ -240,8 +240,7 @@ def login_page():
         background-image: url("https://raw.githubusercontent.com/Ahsan3991/StockPortFolioManager/refs/heads/testing/assets/wealthwise-logo-zip-file/background-image.png");
         background-size: cover;
         background-position: center;
-        position: relative;
-        background: rgba(0, 0, 0, 0.1);
+        background: rgba(0, 0, 0, 0.4);
         
 
     }}
