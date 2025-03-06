0e8d0b4e15de06432fc95b9bc9865eed26a71266
Ahsan3991 <ahsanbashir2012@gmail.com>
2025-03-06 15:46:05 +0100

    try2

--- main.py
+++ main.py
@@ -374,7 +374,7 @@ def login_page():
     
     # Display logo
     # Create a centered container for the logo
-    col1, col2, col3 = st.columns([1, 3, 1])
+    col1, col2, col3 = st.columns([1, 2, 1])
   
     with col1: 
         st.write(" ")
